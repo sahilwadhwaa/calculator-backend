8d04076cb3f68e45873ffbb632bb8925fb1799eb
sahilwadhwaa <sahilwadhwa.5454@gmail.com>
2023-05-17 17:02:24 +0530

    Deleting a calculation now works

--- controllers/calc.js
+++ controllers/calc.js
@@ -1,5 +1,6 @@
 import History from "../model/calculation.js"
 
+//POST
 export const saveCalculation= async (req, res) => {
     try{
         const {name, calculation, result}= req.body;
@@ -15,12 +16,25 @@ export const saveCalculation= async (req, res) => {
     }
 }
 
-/* READ */
+/* GET */
 export const getCalculation = async (req, res) => {
     try {
-      const history = await History.find();
+      const history = await History.find().limit(10);
       res.status(200).json(history);
     } catch (err) {
       res.status(404).json({ message: err.message });
-    }
-  };
+  }
+};
+
+//PATCH
+
+export const addRemoveCalc= async (req, res) => {
+  try{
+    const calculation= await History.findByIdAndDelete(req.params.id)
+    if (!calculation) response.status(404).send("No calculation found");
+    const history = await History.find().limit(10);
+    res.status(200).json(history);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+}
